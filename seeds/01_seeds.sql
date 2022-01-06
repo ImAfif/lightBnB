f6670b49ee35696f255501b3a767e0f6ac84df56
ImAfif <afifadelayoub@gmail.com>
2022-01-06 08:54:17 +0000

    added various tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,38 @@
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Garrott Demange', 'gdemange0@google.ca', 'jcb', '3528374769245784', 1290.74);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Nonnah Tomasian', 'ntomasian1@nymag.com', 'maestro', '675984446299084933', 1568.65);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Dore Zoppie', 'dzoppie2@ustream.tv', 'americanexpress', '374288966839366', 974.97);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Kaylyn Georgeau', 'kgeorgeau3@netscape.com', 'jcb', '3536256241602510', 1911.14);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Quent Deroche', 'qderoche4@forbes.com', 'americanexpress', '346358329445790', 302.61);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Rora Manginot', 'rmanginot5@amazon.com', 'jcb', '3537483247316627', 692.63);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Joellyn Heibel', 'jheibel6@mit.edu', 'jcb', '3567936590125583', 2207.74);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Padriac Tonner', 'ptonner7@usda.gov', 'diners-club-enroute', '201952764756630', 332.86);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Hurlee Salle', 'hsalle8@livejournal.com', 'mastercard', '5108755327788336', 1092.83);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Imogene Stucke', 'istucke9@sina.com.cn', 'bankcard', '5602230870938952', 1537.29);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Kelli Grimm', 'kgrimma@craigslist.org', 'jcb', '3579551077943147', 1088.79);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Wendall Knotton', 'wknottonb@goodreads.com', 'visa', '4041377290853', 1070.84);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Marj Clipston', 'mclipstonc@businesswire.com', 'jcb', '3534751726169284', 2157.74);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Bondon Frost', 'bfrostd@netvibes.com', 'instapayment', '6373956690881023', 192.75);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Hagan Klemt', 'hklemte@a8.net', 'jcb', '3541614514388428', 1046.90);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Sue Hallwell', 'shallwellf@godaddy.com', 'visa-electron', '4913428851164898', 701.87);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Imogen Lissandrini', 'ilissandrinig@printfriendly.com', 'jcb', '3563023594970969', 555.42);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Rani Mattingson', 'rmattingsonh@alexa.com', 'jcb', '3588791227553102', 683.81);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Niel Mawby', 'nmawbyi@soup.io', 'jcb', '3528769589007894', 1801.56);
+insert into users (password, name, email, payment_type, payment_info, amount_due) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','Joannes Nano', 'jnanoj@google.com.br', 'jcb', '3561192557212289', 2308.74);
+
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, country, street, city, province, post_code, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ('25', 'Family home', 'Random house in the suburbs', 'this is a thumbnail photo url', 'this is a cover photo url', 87, 'Canada', '150 King John Smith Street', 'Ottawa', 'ON', '31337', 2, 1, 1);
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, country, street, city, province, post_code, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ('27', 'Cottage', 'Cottage in the wilderness. Nearby massive firepit (sign liability and waivers), lake, and various outdoor activities equipment', 'this is a thumbnail photo url', 'this is a cover photo url', 197, 'Canada', '1391 Gatineau park', 'Gatineau', 'QC', 'Q89 C91', 12, 3, 4);
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, country, street, city, province, post_code, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ('28', 'Apartment', 'This is a newly renovated tenant storage locker created to be your new apartment!!! Apartment in downtown Vancouver!! As convenient as ever in the heart of downtown Vancouver!!! Act now as we are booking up quick!', 'this is a thumbnail photo url', 'this is a photo of storage locker created to be a small apartment', 400, 'Canada', '10 Riverside', 'Vancouver', 'BC', 'BC1 BC1', 0, 0, 0);
+
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id) VALUES ('2021-12-31', '2022-01-02', '3', '25');
+INSERT INTO reservations (start_date, end_date, property_id, guest_id) VALUES ('2021-12-24', '2022-01-15', '4', '27');
+INSERT INTO reservations (start_date, end_date, property_id, guest_id) VALUES ('2021-10-05', '2021-12-15', '5', '28');
+
+
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) VALUES ('28', '5', '61', '5', 'Best apartment in Vancouver!');
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) VALUES ('27', '4', '66', '5', 'I like the area');
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) VALUES ('25', '3', '70', '4', 'Hosts were friendly');
